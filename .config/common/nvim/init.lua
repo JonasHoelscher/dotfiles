6b59305377b73ae53a6a9eb77c02cd3305ece878
Jonas Hölscher <jonas.hoelscher@gmx.de>
2026-02-06 09:38:30 +0100

    nvim: Fix python auto-indentation

--- .config/common/nvim/init.lua
+++ .config/common/nvim/init.lua
@@ -67,3 +67,13 @@ require("config.lazy")
 -- Load external scripts
 require("keymaps")
 require("autocommands")
+
+-- Python stuff
+vim.g.python_indent = {
+  disable_parentheses_indenting = false,
+  closed_paren_align_last_line = false,
+  searchpair_timeout = 150,
+  continue = "shiftwidth()",
+  open_paren = "shiftwidth()",
+  nested_paren = "shiftwidth()",
+}
