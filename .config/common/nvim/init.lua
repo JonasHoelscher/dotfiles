6b6960550413ae270a31938e40b6cf4002ef19e3
Jonas Hölscher <jonas.hoelscher@gmx.de>
2026-03-15 14:10:29 +0100

    nvim: Rework config to smaller one

--- .config/common/nvim/init.lua
+++ .config/common/nvim/init.lua
@@ -1,85 +1,330 @@
---[[
-====================
-Neovim configuration
-main file
-====================
---]]
-
--- Important general settings
-
--- Suppress deprecated warnings
-vim.deprecate = function() end
-
--- Set leader
 vim.g.mapleader = " "
-vim.g.maplocalleader = " "
 
--- File settings
-vim.opt.fileformat = "unix"
-vim.opt.encoding = "utf-8"
-vim.opt.expandtab = true
-vim.opt.undofile = true -- Save undo history
-vim.cmd([[
-	filetype plugin indent on
-	syntax enable
-]])
-
--- Editor settings
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.breakindent = true
-vim.opt.ignorecase = true -- Case insensitive searching
-vim.opt.smartcase = true
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-vim.opt.cursorline = true
-vim.opt.scrolloff = 4
-vim.opt.signcolumn = "yes"
-vim.opt.winwidth = 80
-
---  Default settings for tabs
---  Settings for all files except the ones listed in autocommands
-vim.opt.expandtab = true -- Tabs as spaces
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.softtabstop = 4
-vim.opt.autoindent = true
-vim.opt.smartindent = true
-
--- Show tabs as >>
+-- basics
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.clipboard = "unnamedplus"
 vim.opt.list = true
+vim.opt.ignorecase = true
+vim.opt.scrolloff = 2
+vim.opt.encoding = "utf-8"
+vim.opt.fileformat = "unix"
+vim.opt.undofile = true
+vim.opt.laststatus = 2
+vim.opt.splitkeep = "screen"
+vim.opt.termguicolors = true
+vim.opt.signcolumn = "yes" -- Remove jumping of signcolumnn
 
--- Other settings
-vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
-vim.opt.updatetime = 300
-vim.opt.timeoutlen = 300
-vim.opt.clipboard = "unnamedplus"
-vim.opt.equalalways = false
-
--- Highlight spaces at end of line
+-- highlight trailing whitespace
 vim.cmd([[highlight ExtraWhitespace ctermbg=red guibg=red]])
 vim.cmd([[match ExtraWhitespace /\s\+$/]])
 vim.cmd([[highlight BadWhitespace ctermbg=red guibg=red]])
 
-vim.o.autoread = true
+-- indentations
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.autoindent = true
 
--- Load 'lazy' plugin manager
-require("config.lazy")
+-- bootstrap plugin manager
+local lazypath = vim.fn.stdpath("data").."/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git","clone","https://github.com/folke/lazy.nvim.git",lazypath
+	})
+end
+vim.opt.rtp:prepend(lazypath)
 
--- Load external scripts
-require("keymaps")
-require("autocommands")
+require("lazy").setup({
+	-- file tree
+	{
+		"nvim-tree/nvim-tree.lua",
+		config = function()
+			require("nvim-tree").setup({})
+			vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle Filetree" })
+		end
+	},
 
--- Python stuff
-vim.g.python_indent = {
-  disable_parentheses_indenting = false,
-  closed_paren_align_last_line = false,
-  searchpair_timeout = 150,
-  continue = "shiftwidth()",
-  open_paren = "shiftwidth()",
-  nested_paren = "shiftwidth()",
-}
+	-- fuzzy finder
+	{
+			"nvim-telescope/telescope.nvim",
+		dependencies = {"nvim-lua/plenary.nvim"},
+		config = function()
+			local t = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>sf", t.find_files, { desc = "Search files" })
+			vim.keymap.set("n", "<leader>sg", t.live_grep, { desc = "Seach file content" })
+			vim.keymap.set("n", "<leader><leader>", t.buffers, { desc = "Search buffers" })
+		end
+	},
 
--- Treesitter stuff
-require('nvim-treesitter.install').prefer_git = false
-require('nvim-treesitter.install').compilers = { "clang", "gcc" }
+	-- keybind helper
+	{
+		"folke/which-key.nvim",
+		event = "VimEnter",
+		opts = {
+			preset = "helix",
+			icon = {
+				mappings = true
+			},
+			spec = {
+				{ "<leader>s", group = "[S]earch" },
+				{ "<leader>d", group = "[D]ocument" }
+			}
+		},
+		keys = {
+			{
+				"<leader>?",
+				function()
+					require("which-key").show({ global = false })
+				end,
+				desc = "Buffer Local Keymaps (which-key)",
+			},
+		},
+	},
+
+	-- mason
+	{
+		"williamboman/mason.nvim",
+		config = function()
+		require("mason").setup()
+
+			local mr = require("mason-registry")
+
+			local servers = { "python-lsp-server", "clangd", "ltex-ls-plus", "cpptools", "rust-analyzer" }
+
+			for _, name in ipairs(servers) do
+				local pkg = mr.get_package(name)
+				if not pkg:is_installed() then
+					pkg:install()
+				end
+			end
+		end
+	},
+
+	-- LSP
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+
+			vim.lsp.config("pylsp",{})
+			vim.lsp.config("clangd",{})
+			vim.lsp.config("ltex_plus",{})
+			vim.lsp.config("cpptools",{})
+			vim.lsp.config("rust_analyzer",{})
+
+			vim.lsp.enable("pylsp")
+			vim.lsp.enable("clangd")
+			vim.lsp.enable("ltex_plus")
+			vim.lsp.enable("cpptools")
+			vim.lsp.enable("rust_analyzer")
+
+			vim.api.nvim_create_autocmd("LspAttach",{
+				callback = function(ev)
+					local opts = {buffer = ev.buf}
+
+					vim.keymap.set("n", "gd", vim.lsp.buf.definition,opts, { desc = "Goto Definition" })
+					vim.keymap.set("n", "gr", vim.lsp.buf.references,opts, { desc = "Goto Reference" })
+					vim.keymap.set("n", "K", vim.lsp.buf.hover,opts, { desc = "Show Definition" })
+					vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename" })
+				end
+			})
+		end
+	},
+
+	-- treesitter
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		opts = {
+			ensure_installed = {
+				"bash",
+				"c",
+				"diff",
+				"html",
+				"lua",
+				"luadoc",
+				"markdown",
+				"markdown_inline",
+				"vim",
+				"vimdoc",
+				"python",
+				"rust",
+				"cpp",
+				"latex",
+			},
+			auto_install = true,
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
+			indent = { enable = true },
+		},
+	},
+
+	{
+	"lervag/vimtex",
+		lazy = false,
+		-- tag = "v2.15", -- uncomment to pin to a specific release
+		init = function()
+			-- VimTeX configuration goes here, e.g.
+			vim.g.vimtex_view_method = "zathura"
+			vim.g.vimtex_view_general_options = "--unique file:@pdf\\#src:@line@tex"
+			vim.g.vimtex_compiler_latexmk = {
+				executable = "latexmk",
+				options = { "-pdf", "-shell-escape", "-synctex=1", "-interaction=nonstopmode" },
+			}
+		end,
+	},
+
+	{
+  "barreiroleo/ltex_extra.nvim",
+		dependencies = { "neovim/nvim-lspconfig" },
+
+		opts = {
+			load_langs = { "de-DE", "en-US" },
+			-- save to .ltex dir
+			path = ".ltex",
+		},
+
+		config = function(_, opts)
+			-- Ltex LSP
+			vim.lsp.config("ltex_plus", {
+				on_attach = function()
+					require("ltex_extra").setup(opts)
+				end,
+
+				settings = {
+					ltex = {
+						checkFrequency = 'save',
+						enabled = { 'markdown', 'plaintex', 'rst', 'tex', 'latex' },
+						language = 'de-DE',
+					},
+				},
+			})
+		end,
+	},
+
+	{
+		"rcarriga/nvim-notify"
+	},
+
+})
+
+-- window naviagation
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+
+-- additional keymaps
+vim.keymap.set("n", "<leader>/", require("telescope.builtin").current_buffer_fuzzy_find, { desc = "Search current file" })
+vim.keymap.set("n", "<leader>sh", require("telescope.builtin").help_tags, { desc= "Seach help" })
+
+-- configure terminal --
+-- state variables
+local term_buf = nil
+local term_win = nil
+
+local function toggle_term()
+		-- if window is open close it
+		if term_win and vim.api.nvim_win_is_valid(term_win) then
+				vim.api.nvim_win_close(term_win, true)
+				term_win = nil
+				return
+		end
+
+		-- if buffer exists open in new split
+		if term_buf and vim.api.nvim_buf_is_valid(term_buf) then
+				vim.cmd("vertical botright split")
+				term_win = vim.api.nvim_get_current_win()
+				vim.api.nvim_set_current_buf(term_buf)
+		else
+				-- if no buffer exists create it
+				vim.cmd("vertical botright vnew | terminal")
+				term_win = vim.api.nvim_get_current_win()
+				term_buf = vim.api.nvim_get_current_buf()
+
+				-- remove terminal from buffer list
+				vim.bo[term_buf].buflisted = false
+		end
+
+		-- settings for terminal window
+		vim.cmd("vertical resize 55")
+		vim.wo[term_win].number = false
+		vim.wo[term_win].relativenumber = false
+		vim.wo[term_win].signcolumn = "no"
+		vim.wo[term_win].winfixwidth = true
+		vim.cmd("startinsert")
+end
+
+-- terminal keymaps
+vim.keymap.set("n", "<leader>t", toggle_term, { desc = "Toggle Terminal" })
+vim.keymap.set("t", "<C-t>", toggle_term, { desc = "Toggle Terminal" })
+vim.keymap.set("t", "<ESC>", [[<C-\><C-n>]])
+vim.keymap.set("t", "<C-h>", [[<C-\><C-n><C-w>h]])
+vim.keymap.set("t", "<C-j>", [[<C-\><C-n><C-w>j]])
+vim.keymap.set("t", "<C-k>", [[<C-\><C-n><C-w>k]])
+vim.keymap.set("t", "<C-l>", [[<C-\><C-n><C-w>l]])
+
+-- Close when typing exit
+vim.api.nvim_create_autocmd("TermClose", {
+		callback = function()
+				if term_win and vim.api.nvim_win_is_valid(term_win) then
+						vim.api.nvim_win_close(term_win, true)
+						term_win = nil
+				end
+				term_buf = nil
+		end
+})
+
+-- more keybinds
+vim.keymap.set("i", "<C-k>", vim.lsp.buf.signature_help, { desc = "Show signature help" })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Show diagnostics in loclist" })
+
+-- highlight when yanking text
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
+
+-- set indentation for lua
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "lua" },
+	callback = function()
+		vim.opt_local.expandtab = false
+		vim.opt_local.tabstop = 2
+		vim.opt_local.shiftwidth = 2
+		vim.opt_local.softtabstop = 2
+	end,
+})
+
+-- file specfics
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "python", "c", "cpp", "rust" },
+	callback = function()
+		vim.treesitter.start()
+		vim.opt_local.colorcolumn = "80"
+	end
+})
+
+-- colorscheme
+vim.cmd("colorscheme habamax")
+
+-- add switch for spell check
+vim.opt.spelllang = { "en" }
+function ToggleSpellLang()
+	local current = vim.opt.spelllang:get()[1]
+	if current == "en" then
+		vim.opt.spelllang = { "de" }
+		require("notify")("Spellcheck set to German")
+	else
+		vim.opt.spelllang = { "en" }
+		require("notify")("Spellcheck set to English")
+	end
+end
+
+vim.keymap.set("n", "<leader>dt", ToggleSpellLang, { desc = "Toggle spelllang (en/de)" })
